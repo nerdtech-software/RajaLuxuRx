f356345c2a7c10422066918cc5737f2e71f68345
nerdtech-software <y*********@gmail.com>
2025-02-06 18:08:14 +0530

    about

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
-import About from "./page/about";
+import About from "./page/About";
 import BookingForm from "./page/BookingForm";
 import ContactPage from "./page/ContactPage";
 import Home from "./page/Home";
